090f8d02a9c1b56f7f330864e06034ad0de0b96b
Timo Glane <timo.glane@gmail.com>
2023-08-07 21:33:52 +0200

    Fine tuning neovim config

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -3,24 +3,24 @@ local builtin = require("telescope.builtin")
 -- Fuzzy find files in current tab
 vim.keymap.set("n", ":Ff", builtin.find_files, {})
 
--- Fuzzy find files known to git in current tab
-vim.keymap.set("n", ":Gf", builtin.live_grep, {})
-
 -- Fuzzy find files in new tab
 vim.keymap.set("n", ":Tf", function()
     vim.api.nvim_command(":tabe")
     builtin.find_files()
 end)
 
+-- Fuzzy find a string in files known to git in current tab
+vim.keymap.set("n", ":Gf", builtin.live_grep, {})
+
 -- Fuzzy find strings of project in current tab
 vim.keymap.set("n", ":Ss", function() 
     -- builtin.grep_string({ search = vim.fn.input("Grep > ") });
-    builtin.grep_string()
+    builtin.grep_string({ search = "" })
 end)
 
 -- Fuzzy find strings of project in new tab
 vim.keymap.set("n", ":Ts", function()
     vim.api.nvim_command(":tabe")
     -- builtin.grep_string({ search = vim.fn.input("Grep > ") });
-    builtin.grep_string()
+    builtin.grep_string({ search = "" })
 end)
